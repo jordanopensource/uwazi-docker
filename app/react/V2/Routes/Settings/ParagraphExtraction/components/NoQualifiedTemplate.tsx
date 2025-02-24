66b75dc177d53e1ac38785d7d7af7d8ff5a52446
josh-huridocs <joshua@huridocs.org>
2025-02-24 14:07:46 +0100

    7640 px results (#7689)
    * PX: update styling of no qualified templates message
    
    * apply new criteria message
    
    * px modal styling update
    
    * [MultiSelectList] hideFilters config
    
    * [MultiSelectList] custom classes for item and item container
    
    * [Px - Extractor Modal] Custom classes update for item and item container
    
    * [Px - Extractor Modal] update styling for step 2
    
    * update translations
    
    * [MultiSelectList] set default item class names
    
    * [Table] set default message when data passed is empty
    
    * [Table] remove displaying of boolean result
    
    * [PX] table, delete modal and pagination
    
    * 7640: px ui updates
    
    * use updated react-router, comment prepared lines for filtering, updated translations
    
    * fix eslint errors
    
    * remove duplicates
    
    * fix and update unit test screenshot
    
    * fix login credentials and empty table
    
    * revert password change

--- app/react/V2/Routes/Settings/ParagraphExtraction/components/NoQualifiedTemplate.tsx
+++ app/react/V2/Routes/Settings/ParagraphExtraction/components/NoQualifiedTemplate.tsx
@@ -2,12 +2,12 @@ import { Translate } from 'app/I18N';
 import React from 'react';
 
 const NoQualifiedTemplatesMessage = () => (
-  <div className="flex flex-col gap-2 items-center text-gray-500">
-    <p className="text-xl">
+  <div className="flex flex-col gap-5 items-center text-gray-500 font-semibold lead text-xs uppercase">
+    <p>
       <Translate>No valid target template available</Translate>
     </p>
     <p>
-      <Translate>Qualified templates should have Rich Text property</Translate>
+      (<Translate>The target template needs at least 1 rich text property</Translate>)
     </p>
   </div>
 );
